7101466094b5da56d7c334c7d924650b3313f7a3
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-05-22 16:12:13 +0200

    create items index and new page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,26 @@
 class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :number)
+  end
 end
