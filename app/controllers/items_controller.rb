b5eb2e14a460a04c62c9d165cb4df2d16c44ba3c
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-05-22 16:58:54 +0200

    item can attach photo with active storage cloudinary hosting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :number)
+    params.require(:item).permit(:name, :price, :number, :photo)
   end
 end
